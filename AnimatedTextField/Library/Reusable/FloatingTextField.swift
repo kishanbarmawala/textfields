b2b7576c5a9ba0eb5c2ad6a6f4103f06e8acc958
Kishan Barmawala <52591655+kishanbarmawala@users.noreply.github.com>
2020-07-27 12:17:31 +0530

    Clear Button position fixed. and now placeholder label can be directly accessible and apply custom font and size and colour to placeholder label.

--- AnimatedTextField/Library/Reusable/FloatingTextField.swift
+++ AnimatedTextField/Library/Reusable/FloatingTextField.swift
@@ -8,151 +8,157 @@
 
 import UIKit
 
+private extension TimeInterval {
+    static let animation250ms: TimeInterval = 0.25
+}
+
+private extension UIColor {
+    static let inactive: UIColor = .gray
+}
+
+private enum Constants {
+    static let offset: CGFloat = 8
+    static let placeholderSize: CGFloat = 14
+}
+
 final class FloatingTextField: UITextField {
-
-    // MARK: - Constants
-
-    private enum Constants {
-        static let duration: TimeInterval = 0.25
-        static let offset: CGFloat = 8
-        static let placeholderSize: CGFloat = 14
-    }
-
+    
     // MARK: - Subviews
-
+    
     private var border = UIView()
-    private var label = UILabel()
-
+    var placeHolderLabel = UILabel()
+    
     // MARK: - Private Properties
-
+    
     private var scale: CGFloat {
         Constants.placeholderSize / fontSize
     }
-
+    
     private var fontSize: CGFloat {
         font?.pointSize ?? 0
     }
-
+    
     private var labelHeight: CGFloat {
         ceil(font?.withSize(Constants.placeholderSize).lineHeight ?? 0)
     }
-
+    
     private var textHeight: CGFloat {
         ceil(font?.lineHeight ?? 0)
     }
-
+    
     private var isEmpty: Bool {
         text?.isEmpty ?? true
     }
-
-    private lazy var textInsets = UIEdgeInsets(
-        top: Constants.offset + labelHeight,
-        left: 0,
-        bottom: Constants.offset,
-        right: 0
-    )
-
+    
+    private var textInsets: UIEdgeInsets {
+        UIEdgeInsets(top: Constants.offset + labelHeight, left: 0, bottom: Constants.offset, right: 0)
+    }
+    
     // MARK: - Initialization
-
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        configure()
+        setupUI()
     }
-
+    
     required init?(coder: NSCoder) {
         super.init(coder: coder)
-        configure()
+        setupUI()
     }
-
+    
     // MARK: - UITextField
-
+    
     override var intrinsicContentSize: CGSize {
         return CGSize(width: bounds.width, height: textInsets.top + textHeight + textInsets.bottom)
     }
-
+    
     override var placeholder: String? {
         didSet {
-            label.text = placeholder
+            placeHolderLabel.text = placeholder
         }
     }
-
+    
     override func layoutSubviews() {
         super.layoutSubviews()
         border.frame = CGRect(x: 0, y: bounds.height - 1, width: bounds.width, height: 1)
         updateLabel(animated: false)
     }
-
+    
     override func textRect(forBounds bounds: CGRect) -> CGRect {
         return bounds.inset(by: textInsets)
     }
-
+    
     override func editingRect(forBounds bounds: CGRect) -> CGRect {
         return bounds.inset(by: textInsets)
     }
-
+    
     override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
         return .zero
     }
-
+    
     override func draw(_ rect: CGRect) {
         super.draw(rect)
-
+        
         guard !isFirstResponder else {
             return
         }
-
-        label.transform = .identity
-        label.frame = bounds.inset(by: textInsets)
+        
+        placeHolderLabel.transform = .identity
+        placeHolderLabel.frame = bounds.inset(by: textInsets)
     }
-
+    
+    override func clearButtonRect(forBounds bounds: CGRect) -> CGRect {
+        let originalRect = super.clearButtonRect(forBounds: bounds)
+        return originalRect.offsetBy(dx: 0, dy: (self.bounds.height/4))
+    }
+    
     // MARK: - Private Methods
-
-    private func configure() {
+    
+    private func setupUI() {
         borderStyle = .none
-
-        border.backgroundColor = .gray
+        
+        border.backgroundColor = .inactive
         border.isUserInteractionEnabled = false
         addSubview(border)
-
-        label.textColor = .gray
-        label.font = font
-        label.text = placeholder
-        label.isUserInteractionEnabled = false
-        addSubview(label)
-
+        
+        placeHolderLabel.textColor = .inactive
+        placeHolderLabel.font = font
+        placeHolderLabel.text = placeholder
+        placeHolderLabel.isUserInteractionEnabled = false
+        addSubview(placeHolderLabel)
+        
         addTarget(self, action: #selector(handleEditing), for: .allEditingEvents)
     }
-
+    
     @objc
     private func handleEditing() {
         updateLabel()
         updateBorder()
     }
-
+    
     private func updateBorder() {
-        let borderColor = isFirstResponder ? tintColor : .gray
-        UIView.animate(withDuration: Constants.duration) {
+        let borderColor = isFirstResponder ? tintColor : .inactive
+        UIView.animate(withDuration: .animation250ms) {
             self.border.backgroundColor = borderColor
         }
     }
-
+    
     private func updateLabel(animated: Bool = true) {
         let isActive = isFirstResponder || !isEmpty
-
-        let offsetX = -label.bounds.width * (1 - scale) / 2
-        let offsetY = -label.bounds.height * (1 - scale) / 2
-
+        
+        let offsetX = -placeHolderLabel.bounds.width * (1 - scale) / 2
+        let offsetY = -placeHolderLabel.bounds.height * (1 - scale) / 2
+        
         let transform = CGAffineTransform(translationX: offsetX, y: offsetY - labelHeight - Constants.offset)
             .scaledBy(x: scale, y: scale)
-
+        
         guard animated else {
-            label.transform = isActive ? transform : .identity
+            placeHolderLabel.transform = isActive ? transform : .identity
             return
         }
-
-        UIView.animate(withDuration: Constants.duration) {
-            self.label.transform = isActive ? transform : .identity
+        
+        UIView.animate(withDuration: .animation250ms) {
+            self.placeHolderLabel.transform = isActive ? transform : .identity
         }
     }
-
 }
